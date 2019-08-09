1bf0d88a2de87febfc4643b503439f6b38e3a38a
Arran Jacques <arranjacques@gmail.com>
2019-08-09 10:36:01 +0100

    Add ts class list utility.

---
+++ utilites/class-list.ts
@@ -0,0 +1,40 @@
+export type ClassList = string | Array<string>;
+
+export default class {
+
+    classList: Array<string> [];
+
+    constructor(classes?: ClassList) {
+        if (classes) {
+            this.addToList(classes);
+        }
+    }
+
+    public add(classes: ClassList, condition: any): void {
+
+        const con = arguments.length === 1 ? true : condition;
+
+        if (typeof con === 'function') {
+            if (con()) {
+                this.addToList(classes);
+            }
+        } else if (con) {
+            this.addToList(classes);
+        }
+    }
+
+    public string(): string {
+        return this.toString();
+    }
+
+    public toString(): string {
+        return this.classList.join(' ').trim();
+    }
+
+    protected addToList(classes: ClassList): void {
+        this.classList = this.classList.concat(
+            Array.isArray(classes) ? classes : [classes]
+        );
+    }
+
+}
