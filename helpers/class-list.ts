f2eee406ef2789c2203e433853d03a8b9ef71471
Arran Jacques <arranjacques@gmail.com>
2019-08-16 09:30:34 +0100

    Export string-uc-first from index.ts.

--- helpers/class-list.ts
+++ helpers/class-list.ts
@@ -1,5 +1,5 @@
 import ClassList, { ClassList as Cl } from '../utilites/class-list';
 
-export function classList(classes?: Cl): ClassList {
+export default function(classes?: Cl): ClassList {
     return new ClassList(classes);
 }
--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { default as arrayRandom }  from './helpers/array-random';
 export { default as arrayProcess }  from './helpers/array-process';
 export { default as hasClass }  from './helpers/has-class';
 export { default as stringRandom }  from './helpers/string-random';
+export { default as stringUcFirst }  from './helpers/string-uc-first';
