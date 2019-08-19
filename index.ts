464af1c9ebc104917b6aa956d82ea834c13431ee
Arran Jacques <arranjacques@gmail.com>
2019-08-19 09:57:07 +0100

    Export class list helper from index.ts.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export { default as arrayRandom }  from './helpers/array-random';
 export { default as arrayProcess }  from './helpers/array-process';
+export { default as classList }  from './helpers/class-list';
 export { default as hasClass }  from './helpers/has-class';
 export { default as stringRandom }  from './helpers/string-random';
 export { default as stringUcFirst }  from './helpers/string-uc-first';
