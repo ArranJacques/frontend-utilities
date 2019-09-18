0c92cc4452b91653c0b56054e9095e90b1456208
Arran Jacques <arranjacques@gmail.com>
2019-09-18 12:37:35 +0100

    Add http query string ts helper function

---
+++ helpers/http-query-string.ts
@@ -0,0 +1,27 @@
+function serialize(obj: { [key: string]: any }, prefix?: string): string {
+
+    const str = [];
+
+    for (const key in obj) {
+
+        if (obj.hasOwnProperty(key)) {
+
+            const k = prefix ? prefix + '[' + key + ']' : key;
+            const v = obj[key];
+
+            const param = (v !== null && typeof v === 'object')
+                ? serialize(v, k)
+                : encodeURIComponent(k) + '=' + encodeURIComponent(v);
+
+            if (param && param !== '') {
+                str.push(param);
+            }
+        }
+    }
+
+    return str.join('&');
+}
+
+export default function (obj: { [key: string]: any }): string {
+    return serialize(obj);
+}
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export { default as arrayProcess }  from './helpers/array-process';
 export { default as bemClassList }  from './helpers/bem-class-list';
 export { default as classList }  from './helpers/class-list';
 export { default as hasClass }  from './helpers/has-class';
+export { default as httpQueryString }  from './helpers/http-query-string';
 export { default as stringSlugify }  from './helpers/string-slugify';
 export { default as stringRandom }  from './helpers/string-random';
 export { default as stringUcFirst }  from './helpers/string-uc-first';
