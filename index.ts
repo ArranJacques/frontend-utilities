bd2cd25a4aa5aedd19c86181bd02fb14074abd35
Arran Jacques <arranjacques@gmail.com>
2020-01-18 11:44:26 +0000

    Add format number ts helper function

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ export { default as arrayRandom }  from './helpers/array-random';
 export { default as arrayProcess }  from './helpers/array-process';
 export { default as bemClassList }  from './helpers/bem-class-list';
 export { default as classList }  from './helpers/class-list';
+export { default as formatNumber }  from './helpers/format-number';
 export { default as hasClass }  from './helpers/has-class';
 export { default as httpQueryString }  from './helpers/http-query-string';
 export { default as isObject }  from './helpers/is-object';
